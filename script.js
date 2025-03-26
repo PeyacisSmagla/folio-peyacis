538c6e110fcda683709e5319032a61d2d7a1b87d
Peyacis Smagla <peyacis03@gmail.com>
2025-03-27 01:19:10 +0530

    completed home page

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
-const modeToggle = document.getElementById('mode-toggle');
+const modeToggle = document.getElementById("mode-toggle");
 const body = document.body;
 
-modeToggle.addEventListener('click' , () => {
-    body.classList.toggle('dark-mode');
-    body.classList.toggle('light-mode');
+modeToggle.addEventListener("click", () => {
+  body.classList.toggle("dark-mode");
+  body.classList.toggle("light-mode");
 
-    if(body.classList.contains('dark-mode')) {
-        modeToggle.innerHTML = '<i class="fas fa-moon"  style="color : #fff;" ></i>';
-        
-    } else {
-        modeToggle.innerHTML = '<i class="fas fa-sun" ></i>';
+  if (body.classList.contains("dark-mode")) {
+    modeToggle.innerHTML =
+      '<i class="fas fa-moon"  style="color : #fff;" ></i>';
+  } else {
+    modeToggle.innerHTML = '<i class="fas fa-sun" ></i>';
+  }
+});
 
-    }
-})
+function scrollDown() {
+  window.scrollBy({ top: 500, behavior: "smooth" });
+}
+
+function handleDownload() {
+  const pdfLink =
+    "https://drive.google.com/uc?export=download&id=1VzmxRMsQ9SY-8yY8qRKydnIvx7lfsVhG";
+
+  const link = document.createElement("a");
+  link.href = pdfLink;
+  link.download = "resume.pdf";
+  link.click();
+}
